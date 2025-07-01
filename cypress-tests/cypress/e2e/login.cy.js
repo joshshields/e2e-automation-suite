9ac077655a5ceb7a5f2ca4e77e69d756be136dd4
Josh Shields <joshshieldss17@gmail.com>
2025-06-30 17:35:45 -0700

    added cypress testing for successful,invalid logins

--- cypress-tests/cypress/e2e/login.cy.js
+++ cypress-tests/cypress/e2e/login.cy.js
@@ -5,3 +5,26 @@ describe('Login Page', () => {
     cy.get('#password').should('exist');
   });
 });
+it('should redirect to dashboard on valid login', () => {
+  cy.visit('http://localhost:51515');
+  cy.get('#username').type('admin');
+  cy.get('#password').type('1234');
+  cy.get('button[type="submit"]').click();
+  cy.url().should('include', '/dashboard.html');
+  cy.contains('Welcome to your dashboard');
+});
+it('should show error message for invalid login', () => {
+  cy.visit('http://localhost:51515');
+  cy.get('#username').type('wronguser');
+  cy.get('#password').type('wrongpass');
+  cy.get('button[type="submit"]').click();
+  cy.get('#message').should('contain', 'Invalid credentials.');
+});
+it('should show success message on valid login', () => {
+  cy.visit('http://localhost:51515');
+  cy.get('#username').type('admin');
+  cy.get('#password').type('1234');
+  cy.get('button[type="submit"]').click();
+  cy.get('#message').should('contain', 'Login successful!');
+  cy.get('#message').should('have.class', 'success');
+});
