d76472249074ad8fd17216b79a07dbe661792194
Josh Shields <joshshieldss17@gmail.com>
2025-07-14 11:31:05 +0200

    Fix Cypress password reset tests by persisting user with localStorage

--- cypress-tests/cypress/e2e/reset.cy.js
+++ cypress-tests/cypress/e2e/reset.cy.js
@@ -6,21 +6,23 @@ describe('Password Reset', () => {
   });
 
   it('should show error for unknown user', () => {
-    cy.visit('http://localhost:51515/reset.html');
+    cy.visit('http://localhost:51515/reset.html', {
+      onBeforeLoad(win) {
+        // Preload localStorage with a different user
+        win.localStorage.setItem("users", JSON.stringify({ someuser: "pass" }));
+      },
+    });
     cy.get('#username').type('notregistered');
     cy.get('button[type="submit"]').click();
     cy.get('#message').should('contain', 'Username not found.');
   });
 
   it('should reset password for registered user', () => {
-    // Register user first
-    cy.visit('http://localhost:51515/register.html');
-    cy.get('#username').type('resetuser');
-    cy.get('#password').type('pass123');
-    cy.get('button[type="submit"]').click();
-
-    // Then reset
-    cy.visit('http://localhost:51515/reset.html');
+    cy.visit('http://localhost:51515/reset.html', {
+      onBeforeLoad(win) {
+        win.localStorage.setItem("users", JSON.stringify({ resetuser: "pass123" }));
+      },
+    });
     cy.get('#username').type('resetuser');
     cy.get('button[type="submit"]').click();
     cy.get('#message').should('contain', 'Password reset successful!');
